d926efb113dc6ac64e0122e995cf3a8376604052
Garrigue Benjamin <Wyveth.pro@gmail.com>
2021-11-22 20:50:50 +0100

    Test New Welcome View

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -156,6 +156,18 @@
   });
 
   /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select("#hero-carousel-indicators")
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+  heroCarouselItems.forEach((item, index) => {
+    (index === 0) ?
+    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+  });
+
+  /**
    * Clients Slider
    */
   new Swiper('.clients-slider', {
