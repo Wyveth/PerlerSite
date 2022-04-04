e1954cff623fabc9284b3482a7bb4b9e77258fa2
Wyveth <93991961+Wyveth@users.noreply.github.com>
2022-04-04 20:35:26 +0200

    Update environment.prod.ts
    Hide Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   firebase: {
     projectId: 'perlerwyveth',
-    appId: '1:707528245165:web:33dd8eb0d6d2a50207eb24',
-    databaseURL: 'https://perlerwyveth-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'perlerwyveth.appspot.com',
-    locationId: 'europe-west',
+    appId: '*****************************',
+    databaseURL: '*****************************',
+    storageBucket: '*****************************',
+    locationId: '*****************************',
     apiKey: '*****************************',
-    authDomain: 'perlerwyveth.firebaseapp.com',
-    messagingSenderId: '707528245165',
-    measurementId: 'G-NXZRD0EQ7C',
+    authDomain: '*****************************',
+    messagingSenderId: '*****************************',
+    measurementId: '*****************************',
   },
   production: true
 };
