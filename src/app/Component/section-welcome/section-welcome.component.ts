12c080037cbaba631bf1621d13d31a86fe57dd2c
Garrigue Benjamin <wyveth.pro@gmail.com>
2025-07-26 18:19:07 +0200

    Mise en place de tailwind + section welcome OK

--- src/app/Component/section-welcome/section-welcome.component.ts
+++ src/app/Component/section-welcome/section-welcome.component.ts
@@ -1,14 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { GalleriaModule } from 'primeng/galleria';
+import { CardModule } from 'primeng/card';
 
 @Component({
     selector: 'app-section-welcome',
     templateUrl: './section-welcome.component.html',
     styleUrls: ['./section-welcome.component.scss'],
     standalone: true,
-    imports: [CommonModule]
+    imports: [CommonModule, GalleriaModule, CardModule]
 })
 export class SectionWelcomeComponent implements OnInit {
+  images: string[] = [
+    'assets/img/Perles-Atelier.jpg',
+    'assets/img/PokemonGen1.jpg',
+    'assets/img/Justice_League.jpg',
+    'assets/img/Pokemon_Starter.jpg'
+  ];
+
+  responsiveOptions: any[] = [
+    {
+        breakpoint: '991px',
+        numVisible: 4
+    },
+    {
+        breakpoint: '767px',
+        numVisible: 3
+    },
+    {
+        breakpoint: '575px',
+        numVisible: 1
+    }
+];
 
   constructor() { }
 
